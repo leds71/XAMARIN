9b65b220b11eddf09c757414a04cbdc9d33dd2d1
leds71 <arturoac@comunidad.unam.mx>
2015-09-29 10:16:25 -0500

    Prueba

--- HolaMundo/HolaMundo/App.cs
+++ HolaMundo/HolaMundo/App.cs
@@ -9,7 +9,7 @@ namespace HolaMundo
 		{	
 			return new ContentPage { 
 				Content = new Label {
-					Text = "Arturo Alvarez Castillo !",
+					Text = "Arturo Alvarez Castillo 99364881!",
 					VerticalOptions = LayoutOptions.CenterAndExpand,
 					HorizontalOptions = LayoutOptions.CenterAndExpand,
 				},
